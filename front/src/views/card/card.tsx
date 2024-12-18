0a7a1c93a0018c7bb7d0586e277e8dbbc9a26d88
agustinMontoya2 <agus7_montoya@hotmail.com>
2024-12-18 17:06:52 -0300

    change description json

--- front/src/views/card/card.tsx
+++ front/src/views/card/card.tsx
@@ -41,10 +41,10 @@ function Card() {
   <div style={{maxHeight: "36rem"}} className="bg-[#FFFFFF] w-[90%] max-w-4xl h-[65%] flex flex-col rounded-xl shadow-hoverCentered">
     {nail ? (
       <div className="w-full h-full flex flex-col justify-evenly items-center">
-        <div className="h-1/6 flex items-center justify-center">
+        <div className="h-1/7 flex items-center justify-center">
         <h4 style={{ fontFamily: "Voltaire" }} className=" text-[#1E90FF] text-3xl">{nail.nombre}</h4>
         </div>
-        <div className="h-4/6 flex flex-col items-center justify-evenly md:flex-row">
+        <div className="h-5/6 flex flex-col items-center justify-evenly md:flex-row">
         <div className="w-full flex flex-col items-center md:w-1/2">
       {nail.imagenes.length > 1 ? (
         // Carrusel para múltiples imágenes
@@ -98,7 +98,9 @@ function Card() {
         </div>
       </div>
     ) : (
+        <div className="h-full flex items-center justify-center">
       <p className="text-center text-gray-700">Producto no encontrado</p>
+        </div>
     )}
   </div>
 </div>
